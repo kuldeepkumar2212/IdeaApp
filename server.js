c00fe0c3dbabd6a567d42416a5027433fffcbca5
kuldeepkumar2212 <kuldeepkumarraju2001@gmail.com>
2023-07-01 17:59:58 +0530

    added the structure(routers,models,controller)

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const serverConfig = require('./configs/server.config');
 //require is used to use the dependencies installed
 const app = express();
 //app is like a central body from which everthing is controlled 
 
 //starting the server
-//3333 is the port where the process is running
-app.listen(3333, ()=>{
-    console.log("server started");
+//port is defined in the server config is the port where the process is running
+app.listen(serverConfig.port, ()=>{
+    console.log(`server started at ${serverConfig.port}`);
 })
